a46b98dfdb6968338a9d6cb573c518ac6ff8fce7
OXKR96 <128667745+OXKR96@users.noreply.github.com>
2023-05-26 20:13:10 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -9,17 +9,12 @@ app.use(cors({
     origin: '*'
 }))
 
-const tipoProyecto = require('./routes/tipoProyecto')
 
-const cliente = require('./routes/cliente')
-const universidad = require('./routes/universidad')
-const etapa = require('./routes/etapa')
+const proyecto = require('./routes/proyecto')
 
 // middlewares
-app.use('/api/tiposproyectos',tipoProyecto)
 
-app.use('/api/clientes',cliente)
-app.use('/api/universidades',universidad)
-app.use('/api/etapas',etapa)
+app.use('/api/proyectos',proyecto)
+
 
 module.exports = app
